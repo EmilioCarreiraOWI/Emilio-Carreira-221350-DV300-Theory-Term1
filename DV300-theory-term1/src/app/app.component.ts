3b928ae85f586a984f217a693d24f9075cf9bf5e
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2024-04-07 14:42:00 +0200

    80% finished

--- DV300-theory-term1/src/app/app.component.ts
+++ DV300-theory-term1/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
 import { CardsComponent } from './component/cards/cards.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, CardsComponent],
+  imports: [RouterOutlet, NavBarComponent, CardsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
